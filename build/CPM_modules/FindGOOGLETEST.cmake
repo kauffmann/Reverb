dba3fcb8d56221328f9a862cab73cd96d4c45601
Michael Kauffmann <kauffmann.michael@gmail.com>
2024-08-06 03:26:44 +0200

    Delete build directory

--- build/CPM_modules/FindGOOGLETEST.cmake
+++
@@ -1,3 +0,0 @@
-include("C:/Users/kauff/Documents/Audio Programming/Cmake JUCE Plugin Template/libs/cpm/CPM_0.38.2.cmake")
-CPMAddPackage("NAME;GOOGLETEST;GITHUB_REPOSITORY;google/googletest;GIT_TAG;v1.14.0;VERSION;1.14.0;SOURCE_DIR;C:/Users/kauff/Documents/Audio Programming/Cmake JUCE Plugin Template/libs/googletest;OPTIONS;INSTALL_GTEST OFF;gtest_force_shared_crt ON")
-set(GOOGLETEST_FOUND TRUE)
--- build/plugin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/kauff/Documents/Audio Programming/Cmake JUCE Plugin Template/plugin
-# Build directory: C:/Users/kauff/Documents/Audio Programming/Cmake JUCE Plugin Template/build/plugin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
